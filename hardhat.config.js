e8e8df5db9f49ee417afa94e1fafce01ee2f0c39
unknown <webexpert0524@gmail.com>
2021-10-28 10:20:32 -0700

    1.1

---
+++ hardhat.config.js
@@ -0,0 +1,87 @@
+require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-abi-exporter");
+require("@nomiclabs/hardhat-solhint");
+require("hardhat-gas-reporter");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+
+// Load environment variables from .env file. Suppress warnings using silent
+// if this file is missing. dotenv will never modify any environment variables
+// that have already been set.
+// https://github.com/motdotla/dotenv
+require('dotenv').config({silent: true});
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+real_accounts = undefined;
+if(process.env.DEPLOYER_KEY && process.env.OWNER_KEY) {
+  real_accounts = [process.env.DEPLOYER_KEY, process.env.OWNER_KEY];
+}
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`,
+      tags: ["test", "legacy", "use_root"],
+      chainId: 3,
+      accounts: ["6354e35a2c2da44f17508b82f73d607217a4f2652e4d9f8e11356c1f6a81eae3"],
+    },
+    fantom: {
+      url: `https://rpcapi.fantom.network`,
+      tags: ["test", "legacy", "use_root"],
+      chainId: 250,
+      accounts: ["6354e35a2c2da44f17508b82f73d607217a4f2652e4d9f8e11356c1f6a81eae3"],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
+      tags: ["legacy", "use_root"],
+      chainId: 1,
+      accounts: real_accounts,
+    }
+  },
+  mocha: {
+  },
+  abiExporter: {
+    path: './build/contracts',
+    clear: true,
+    flat: true,
+    spacing: 2
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10000,
+          }
+        }
+      }
+    ]
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    owner: {
+      default: 1,
+    },
+  },
+};
+
